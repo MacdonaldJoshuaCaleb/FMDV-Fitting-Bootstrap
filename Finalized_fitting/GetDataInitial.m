90227822f1864d9cee0c4302d4055a8eb7d638c8
J.C. Macdonald <72512262+MacdonaldJoshuaCaleb@users.noreply.github.com>
2024-02-26 21:39:00 +0200

    add comments to GetDataInitial.m

--- Finalized_fitting/GetDataInitial.m
+++ Finalized_fitting/GetDataInitial.m
@@ -1,8 +1,11 @@
 function [d] = GetDataInitial(fit,nLevel)
+% generate new initial data values according to hetrodesic noise structure given a specified noise level
+% returns generated initial data point 
     d = zeros(1,length(fit));
         for j = 1:length(fit)
             d(j) = fit(j) + normrnd(0,fit(j)*nLevel(1).^2);
             while d(j) < 0
+            % resample if value is negative
                 d(j) = fit(j) + normrnd(0,fit(j)*nLevel(1).^2);
             end
        
