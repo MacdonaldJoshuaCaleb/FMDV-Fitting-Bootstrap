3251e6accec4ae2edd24f60a20354103cb69582c
J.C. Macdonald <joshuamac@tauex.tau.ac.il>
2024-02-27 10:50:24 +0200

    add comments to program files

--- Finalized_fitting/ViralGrowth.m
+++ Finalized_fitting/ViralGrowth.m
@@ -1,6 +1,12 @@
 function vpfit = ViralGrowth(ses,tt,s_idx)
+% input 
+% viral load data - ses
+% observation times - tt
+% index of first positive viral load measure - s_idx
 
+% return estiamte for P0 and r
 
+% objective function, assumes simple exponential growth
 function yy = paramfun1(p,t)
     x0 = p(1);
     r = p(2);
@@ -9,6 +15,7 @@ function yy = paramfun1(p,t)
    yy = f(t);
 end 
 
+% truncate to only use up to first postive measured load 
 virus = ses;
     I = s_idx;
     virus = virus(1:I);
@@ -17,12 +24,13 @@ virus = ses;
         virus = [virus(1),virus(I)];
         tts = [tt(1),tt(I)];
     end
+    % define time stepping, lower, and upper bounds, and initial guess
     t = linspace(tts(1),tts(end));
     lb = [.005, 1];
     ub = [1,12];
     p0 = [.01,4];
     
-
+    % fit the data 
     [vpfit resnorm] = lsqcurvefit(@paramfun1,p0,tts,virus,lb,ub);
 
 
